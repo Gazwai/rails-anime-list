785f808f2af8be15b743d7919c40910de5017318
Gary Y <garyyau080@gmail.com>
2024-12-13 15:38:52 +0900

    refactor: add unique index for mal_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_13_061840) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_13_063723) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -22,6 +22,7 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_13_061840) do
     t.integer "mal_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["mal_id"], name: "index_animes_on_mal_id", unique: true
   end
 
   create_table "bookmarks", force: :cascade do |t|
