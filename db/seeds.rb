72c829ef4da023acef56af9a0b0bf32939cdd7ee
Gary Y <garyyau080@gmail.com>
2024-12-13 16:38:44 +0900

    refactor: Make seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,24 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require "open-uri"
+require "json"
+
+puts "Cleaning up database..."
+Anime.destroy_all
+puts "Database cleaned"
+
+url = "https://api.jikan.moe/v4/top/anime"
+
+2.times do |i|
+  puts "Importing anime from page #{i + 1}"
+  animes = JSON.parse(URI.open("#{url}?page=#{i + 1}").read)["data"]
+  animes.each do |anime|
+    puts "Creating #{anime["title"]}"
+    Anime.create(
+      image_url: anime["images"]["webp"]["image_url"],
+      mal_id: anime["mal_id"],
+      overview: anime["synopsis"],
+      rating: anime["score"],
+      title: anime["title"],
+    )
+  end
+end
+puts "Animes created"
