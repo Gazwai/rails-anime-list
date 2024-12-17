38dc80b63bcbba5bf3ffc5948b312d53630b0dc8
Gary Y <garyyau080@gmail.com>
2024-12-18 00:27:51 +0900

    feat: Add index and show page for anime

--- app/controllers/animes_controller.rb
+++ app/controllers/animes_controller.rb
@@ -1,7 +1,17 @@
 class AnimesController < ApplicationController
   allow_unauthenticated_access only: [ :index ]
+  before_action :set_anime, only: [ :show]
 
   def index
-    @animes = Anime.all
+    @top_anime = Anime.order(ranking: :asc).limit(50)
+  end
+
+  def show
+  end
+
+  private
+
+  def set_anime
+    @anime = Anime.find(params[:id])
   end
 end
