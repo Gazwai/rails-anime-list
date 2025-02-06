fd3aafd35ec597fd09c5534137dee163a2f65c97
gary <gary.yau@hotmail.com>
2025-02-06 21:16:58 +0900

    chore: clean up and add presence validation

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -3,11 +3,9 @@ class Bookmark < ApplicationRecord
   belongs_to :anime
 
   validates :comment, length: { maximum: 300 }
-  validates :anime_id, uniqueness: { scope: :list_id, message: "is already in the list" }
+  validates :anime_id, uniqueness: { scope: :list_id, message: "is already in the list" }, presence: true
 
   after_update_commit :broadcast_message
-
-  # -> { broadcast_replace_to "bookmark", partial: "lists/comment", locals: { bookmark: self }, target: self.id }
 
   private
 
